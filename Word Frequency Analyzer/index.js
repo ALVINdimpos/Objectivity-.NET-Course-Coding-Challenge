03444e1887cd423fce76568a3452b0bf880cc8e4
ALVINdimpos <alvindimpos@gmail.com>
2023-03-21 20:46:53 +0200

    Refactor quiz application to use the readline module for terminal input/output instead of prompt() and alert() for web browser. Improve code readability and add comments for clarity.

---
+++ Word Frequency Analyzer/index.js
@@ -0,0 +1,23 @@
+function analyzeText(text) {
+  // Remove all non-word characters and convert to lowercase
+  const words = text.toLowerCase().match(/\b\w+\b/g);
+  
+  // Create an object to store the frequency count of each word
+  const wordFrequency = {};
+  for (const word of words) {
+    wordFrequency[word] = (wordFrequency[word] || 0) + 1;
+  }
+  
+  // Convert the word frequency object to an array of {word, frequency} objects
+  const wordFrequencyArray = Object.entries(wordFrequency).map(([word, frequency]) => ({ word, frequency }));
+  
+  // Sort the array by frequency in descending order
+  wordFrequencyArray.sort((a, b) => b.frequency - a.frequency);
+  
+  return wordFrequencyArray;
+}
+
+const text = "This is a sample text. It contains some words that will be analyzed.";
+const wordFrequency = analyzeText(text);
+console.log(wordFrequency);
+
